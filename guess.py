0631e9d544e19beaa31eaf42423b7326fde92ada
Chris Martinz <chmartinez2014@gmail.com>
2020-08-12 21:29:25 -0500

    Added feature
    Included a 'play again' prompt

--- guess.py
+++ guess.py
@@ -2,20 +2,31 @@ from random import randint
 
 print('Welcome to the number guessing game!')
 
-attempts = 0
-
-random_number = randint(0,10)
-
-while attempts != 10:
-    attempts += 1
-    number_guessed = input("Guess a number 1-10 ")
-    if int(number_guessed) > int(random_number):
-        print("Your number is too high")
-    elif int(number_guessed) < int(random_number):
-        print("Your number is too low")
-    elif int(number_guessed) == int(random_number):
-        print("Congrats!! You guessed the right number")
-        print("You took {} tries to guess the right number".format(attempts))
-        break
-    else:
-        print("Please try again")
+def start_game():
+    attempts = 0
+    random_number = randint(1,10)
+    while True:
+        guess = input("Guess a number 1-10  ")
+        attempts += 1
+        try:
+            int(guess)
+        except ValueError:
+            print("Oops, thats not a number")
+            continue
+        if int(guess) > random_number:
+            print("It's lower")
+        elif int(guess) < random_number:
+            print("It's higher!")
+        elif int(guess) == random_number:
+            print("Congrats!! You guessed the right number")
+            print("You took {} tries to guess the right number.".format(attempts))
+            ask = input("Would you like to play again?  (Yes/No)  ")
+            if ask.upper() == "YES":
+                start_game()
+            elif ask.upper() == "NO":
+                print("Thanks for playing!")
+                quit()
+            else:
+                print("I'll take that as a yes!")
+                continue
+start_game()
