0dbc953e1626b540a3b50b5bbcd7b427878ee027
Chris Martinz <chmartinez2014@gmail.com>
2020-08-13 12:34:36 -0500

    Finished!
    Cleaned up

--- guess.py
+++ guess.py
@@ -1,32 +1,33 @@
 from random import randint
 
-print('Welcome to the number guessing game!')
+
 
 def start_game():
+    print('Welcome to the number guessing game!')
     attempts = 0
     random_number = randint(1,10)
     while True:
-        guess = input("Guess a number 1-10  ")
         attempts += 1
         try:
-            int(guess)
+            guess = int(input("Guess a number 1-10  "))
         except ValueError:
             print("Oops, thats not a number")
-            continue
-        if int(guess) > random_number:
-            print("It's lower")
-        elif int(guess) < random_number:
-            print("It's higher!")
-        elif int(guess) == random_number:
-            print("Congrats!! You guessed the right number")
-            print("You took {} tries to guess the right number.".format(attempts))
-            ask = input("Would you like to play again?  (Yes/No)  ")
-            if ask.upper() == "YES":
-                start_game()
-            elif ask.upper() == "NO":
-                print("Thanks for playing!")
-                quit()
-            else:
-                print("I'll take that as a yes!")
-                continue
+        else:
+            if guess > 10:
+                print("Whoops thats not a number 1-10!")
+            elif guess > random_number:
+                print("It's lower!")
+            elif guess < random_number:
+                print("It's higher!")
+            elif guess == random_number:
+                print("Congrats!! You guessed the right number")
+                print("You took {} tries to guess the right number.".format(attempts))
+                ask = input("Would you like to play again?  (Yes/No)  ")
+                if ask.upper() == "YES":
+                    start_game()
+                elif ask.upper() == "NO":
+                    print("Thanks for playing!")
+                    quit()
+                else:
+                    quit()
 start_game()
